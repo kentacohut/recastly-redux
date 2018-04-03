3de46223661aab5138ea9183f3f7bfc742386627
Steve Hallam <StevenAHallam@gmail.com>
2018-04-03 16:02:31 -0700

    Replaced React components with Redux Containers

--- src/components/App.js
+++ src/components/App.js
@@ -8,37 +8,20 @@ import changeVideo from '../actions/currentVideo.js';
 import changeVideoList from '../actions/videoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import store from '../store/store.js';
+import handleVideoSearch from '../actions/search.js';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      videos: [],
-      currentVideo: null
-    };
+    // this.state = {
+    //   videos: [],
+    //   currentVideo: null
+    // };
   }
 
   componentDidMount() {
-    this.getYouTubeVideos('react tutorials');
-  }
-
-  handleVideoListEntryTitleClick(video) {
-    this.setState({currentVideo: video});
-  }
-
-  getYouTubeVideos(query) {
-    var options = {
-      key: this.props.API_KEY,
-      query: query
-    };
-
-    this.props.searchYouTube(options, (videos) =>
-      this.setState({
-        videos: videos,
-        currentVideo: videos[0]
-      })
-    );
+    handleVideoSearch('react tutorials')(store.dispatch);
   }
 
   //TODO: swap out the React components below for the container components
@@ -46,16 +29,13 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Nav handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
+        <Nav/>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer/>
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer/>
           </div>
         </div>
       </div>
