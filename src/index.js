82046045648873a0e8b1ac16c44037ba8dc5c0b6
Steve Hallam <StevenAHallam@gmail.com>
2018-04-02 18:58:08 -0700

    Implemented actions, reducers, and store.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import React from 'react';
 import App from './components/App.js';
 import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 import store from './store/store.js';
 //TODO: Import the Provider component from 'react-redux' here!
-
+import { Provider } from 'react-redux';
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
 
-ReactDOM.render(
-    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
+render(
+  <Provider store={store}>
+    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube}/>
+  </Provider>,
   document.getElementById('app')
 );
